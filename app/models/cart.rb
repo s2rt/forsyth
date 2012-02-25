81a462aeeb28304b65560d90e12bd6dfabb0e3d5
Stuart Forsyth <swforsyth@gmail.com>
2012-02-25 16:56:37 +1100

    Making purchases relevant
    In terms of cups of coffee.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,7 +5,9 @@ class Cart < ActiveRecord::Base
   def add_product(product_id)
    	current_item = line_items.find_by_product_id(product_id)
    	if current_item
-   	  current_item.quantity += 1
+   	  #digital goods, don't let the quantity get higher than 1
+   	  current_item.quantity = 1
+   	  #current_item.quantity += 1
    	else
    	  current_item = line_items.build(product_id: product_id)
    	end
@@ -14,5 +16,14 @@ class Cart < ActiveRecord::Base
  	
  	def total_price
  	  line_items.to_a.sum{|item| item.total_price}
+  end
+  
+  def cups_of_coffee
+    latte_price = 4
+    if self.total_price > 0
+      return (self.total_price / latte_price*20).round/20.0
+    else
+     return 0
+    end 
   end
 end
